8c6610bf54728e73510e01f6cb44e847c058d328
Svanberg <ek.svanberg@gmail.com>
2024-11-03 16:08:14 +0100

    Jda

--- Pygame get started.py/main.py
+++ Pygame get started.py/main.py
@@ -1,31 +1,40 @@
 import pygame
+import math
 
 pygame.init()
 
+# start
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 player = pygame.Rect((300, 250, 50, 50))
-
 running = True
+
+speed = 1
+
 while running:
 
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
 
     pygame.draw.rect(screen, (255, 0, 0), player)
 
     key = pygame.key.get_pressed()
-    if key[pygame.K_a] == True:
-        player.move_ip(-1,0)
-    elif key[pygame.K_d] == True:
-        player.move_ip(1,0)
-    elif key[pygame.K_w] == True:
-        player.move_ip(0,-1)
-    elif key[pygame.K_s] == True:
-        player.move_ip(0,1)
+    
+    move_x = 0
+    move_y = 0
+    
+    if key[pygame.K_a]:
+        move_x = -1
+    if key[pygame.K_d]:
+        move_x = 1
+    if key[pygame.K_w]:
+        move_y = -1
+    if key[pygame.K_s]:
+        move_y = 1
+    
+    player.move_ip(move_x * speed, move_y * speed)
 
+    # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
